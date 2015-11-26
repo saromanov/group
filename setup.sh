6084a95e6a6f921e65dec3d243fb69f19acc81e6
Sergey Romanov <xxsmotur@gmail.com>
2015-11-27 02:53:02 +0500

    Setup.sh: Update

--- setup.sh
+++ setup.sh
@@ -3,9 +3,11 @@
 VER=`python -c 'import sys; print("".join(map(str, sys.version_info[0:2])))';`
 VER27="27"
 if [[ $VER != "35" ]]; then
+    echo "Execution of pylint"
     pylint --disable=missing-docstring,invalid-name,fixme ./groups/*.py
 fi
 
 if [[ $VER != "36" ]]; then
+    echo "Execution of flake"
     flake8 .
 fi
